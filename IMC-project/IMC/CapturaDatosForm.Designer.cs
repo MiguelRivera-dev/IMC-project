d42031d25327c33c7c067781543a7b6160709410
Sebollo9528 <135871881+Sebollo9528@users.noreply.github.com>
2026-02-07 13:44:06 -0600

    Add files via upload

---
+++ IMC-project/IMC/CapturaDatosForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace IMC
+{
+    partial class CapturaDatosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.rbtMasculino = new System.Windows.Forms.RadioButton();
+            this.rbtFemenino = new System.Windows.Forms.RadioButton();
+            this.grpGenero = new System.Windows.Forms.GroupBox();
+            this.grpGenero.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(38, 9);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(31, 13);
+            this.lblPeso.TabIndex = 0;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(38, 55);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(41, 167);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(144, 47);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click_1);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(125, 9);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 3;
+            this.txtPeso.TextChanged += new System.EventHandler(this.txtPeso_TextChanged);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(125, 52);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 4;
+            // 
+            // rbtMasculino
+            // 
+            this.rbtMasculino.AutoSize = true;
+            this.rbtMasculino.Location = new System.Drawing.Point(0, 0);
+            this.rbtMasculino.Name = "rbtMasculino";
+            this.rbtMasculino.Size = new System.Drawing.Size(68, 17);
+            this.rbtMasculino.TabIndex = 5;
+            this.rbtMasculino.TabStop = true;
+            this.rbtMasculino.Text = "Maculino";
+            this.rbtMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbtFemenino
+            // 
+            this.rbtFemenino.AutoSize = true;
+            this.rbtFemenino.Location = new System.Drawing.Point(0, 37);
+            this.rbtFemenino.Name = "rbtFemenino";
+            this.rbtFemenino.Size = new System.Drawing.Size(71, 17);
+            this.rbtFemenino.TabIndex = 6;
+            this.rbtFemenino.TabStop = true;
+            this.rbtFemenino.Text = "Femenino";
+            this.rbtFemenino.UseVisualStyleBackColor = true;
+            // 
+            // grpGenero
+            // 
+            this.grpGenero.Controls.Add(this.rbtMasculino);
+            this.grpGenero.Controls.Add(this.rbtFemenino);
+            this.grpGenero.Location = new System.Drawing.Point(41, 90);
+            this.grpGenero.Name = "grpGenero";
+            this.grpGenero.Size = new System.Drawing.Size(200, 60);
+            this.grpGenero.TabIndex = 7;
+            this.grpGenero.TabStop = false;
+            this.grpGenero.Text = "groupBox1";
+            // 
+            // CapturaDatosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grpGenero);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Name = "CapturaDatosForm";
+            this.Text = "CalculateForm";
+            this.grpGenero.ResumeLayout(false);
+            this.grpGenero.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.RadioButton rbtMasculino;
+        private System.Windows.Forms.RadioButton rbtFemenino;
+        private System.Windows.Forms.GroupBox grpGenero;
+    }
+}
---
+++ IMC-project/IMC/CapturaDatosForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class CapturaDatosForm : Form
+    {
+        public CapturaDatosForm()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(30, 30, 30); // Fondo oscuro
+            ConfigurarInputs();
+        }
+
+        private void ConfigurarInputs()
+        {
+            // Estilo para las etiquetas
+            EstilizarLabel(lblPeso, "Peso (kg):");
+            EstilizarLabel(lblAltura, "Altura (m):");
+
+            // Estilo para el botón calcular
+            btnCalcular.FlatStyle = FlatStyle.Flat;
+            btnCalcular.BackColor = Color.FromArgb(255, 87, 34); // Naranja llamativo
+            btnCalcular.ForeColor = Color.White;
+            btnCalcular.Font = new Font("Segoe UI", 14, FontStyle.Bold);
+            btnCalcular.Cursor = Cursors.Hand;
+        }
+
+        private void EstilizarLabel(Label lbl, string texto)
+        {
+            lbl.Text = texto;
+            lbl.ForeColor = Color.LightGray;
+            lbl.Font = new Font("Segoe UI", 12, FontStyle.Regular);
+        }
+
+        // Lógica del botón Calcular
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double peso = double.Parse(txtPeso.Text);
+                double altura = double.Parse(txtAltura.Text);
+
+                // Validamos que no metan ceros
+                if (altura == 0) { MessageBox.Show("La altura no puede ser 0"); return; }
+
+                double imc = peso / (altura * altura);
+
+                // LLAMAMOS AL FORM DINÁMICO
+                ResultadoForm formRes = new ResultadoForm(imc);
+                formRes.Show();
+                this.Hide();
+            }
+            catch
+            {
+                MessageBox.Show("Por favor ingresa números válidos.");
+            }
+        }
+
+        private void btnCalcular_Click_1(object sender, EventArgs e)
+        {
+
+            // Sacamos el valor de las cajas de texto para el calculo
+            float weight;
+            float height;
+
+            // Con este if verificamos que los valores del peso sean validos
+            if (float.TryParse(txtPeso.Text, out weight))
+            {
+                // Con este if verificamos que los valores de la altura sean validos
+                if (float.TryParse(txtAltura.Text, out height))
+                {
+                    // Formula para calcular el imc
+                    double imc = weight / (height * height);
+                    // Verificacion de los resultados para ver el camino a seguir
+                    
+                    // if para el imc bajo
+                    if (imc < 18.49)
+                    {
+                        ImcBajo imcBajo = new ImcBajo();
+                        imcBajo.Show();
+
+                    }   // if para el imc normal
+                    else if (imc > 18.49 && imc < 24.99)
+                    {
+                        imcNormal Normal = new imcNormal();
+                        Normal.Show();
+                    }
+                    else   // else para el imc alto
+                    {
+                        imcAlto Alto = new imcAlto();
+                        Alto.Show();
+                    }
+
+                    // Cierra la ventana anterior osea la de este codigo
+                    this.Close();
+                }
+                else // El else verifica que la altura sea un valor valido
+                {
+                    MessageBox.Show("Ingresa un valor valido");
+                }
+            }
+            else // El else verifica que el peso sea un valor valido
+            {
+                MessageBox.Show("Ingresa un valor valido");
+            }
+
+            
+            
+            
+        }
+
+        private void txtPeso_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
